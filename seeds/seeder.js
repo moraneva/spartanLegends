f37c6e3878deeaec21a3f4ce16c2062e30126df2
harazimb <harazimb@msu.edu>
2016-02-27 04:51:05 -0500

    Facebook image

--- seeds/seeder.js
+++ seeds/seeder.js
@@ -34,6 +34,8 @@ var data = [
             },
             {
                 'name': 'Facebook',
+                ' large_img_path': 'facebook.png',
+
             },
             {
                 'name': 'Google',
