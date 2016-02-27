1983b9220fff40bb040f643539d8acad3f423c14
Zachary Rosenthal <rosent76@msu.edu>
2016-02-27 01:00:31 -0500

    working on auth

---
+++ routes/auth.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zacharyrosenthal on 2/27/16.
+ */
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+
+
+/* GET home page. */
+router.post('/login', function(req, res) {
+
+    
+
+});
+
+router.get('/logout', function(req, res) {
+
+});
+
+module.exports = router;
