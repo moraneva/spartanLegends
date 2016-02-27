9f9d0d8939558e65578a605e11331042ee940488
Evan Moran <moraneva@msu.edu>
2016-02-27 08:44:43 -0500

    fixing company controller

--- public/js/script.js
+++ public/js/script.js
@@ -8,19 +8,15 @@ app.config(function ($routeProvider) {
     $routeProvider.when('/home', {
         templateUrl: '/views/home.html',
         controller: 'myCtrl'
-        }).when('/login', {
-            templateUrl: '/views/login.html'
-        }).
+    }).when('/login', {
+        templateUrl: '/views/login.html'
+    }).when('/companies', {
+        templateUrl: '/views/companies.html',
+        controller: 'companyListController'
+    }).otherwise({
+        redirectTo: '/companies'
 
-        when('/companies', {
-            templateUrl: '/views/companies.html',
-            controller: 'companiesCtrl'
-        }).
-
-        otherwise({
-            redirectTo: '/companies'
-
-        });
+    });
 
 });
 
@@ -35,7 +31,10 @@ app.controller('myCtrl', function ($scope) {
     $scope.likes = 0;
     $scope.dislikes = 0;
     $scope.percent = $scope.getPercent();
-    $scope.comments =[{user:"Zach", comment: "That progress bar is better than anything I have ever programmed"},{user:"Ryan", comment:"Great idea!"}];
+    $scope.comments = [{
+        user: "Zach",
+        comment: "That progress bar is better than anything I have ever programmed"
+    }, {user: "Ryan", comment: "Great idea!"}];
 
     $scope.liked = function () {
 
@@ -49,27 +48,13 @@ app.controller('myCtrl', function ($scope) {
         $scope.percent = $scope.getPercent();
     };
 
-    $scope.addComment = function() {
+    $scope.addComment = function () {
 
         $scope.comments.push({
             user: "Ryan",
             comment: $scope.userComment
         });
 
-
     }
-});
-
-
-app.controller('companiesCtrl', function ($scope) {
-
-    $scope.companies =
-        [
-            {name:'Facebook', icon:"fa-facebook-official"},
-            {name:'Twitter', icon: "fa-twitter" },
-            {name:"Slack", icon:"fa-slack"},
-            {name:"Google", icon: "fa-google"},
-            {name: "Amazon", icon:"fa-amazon"}
-        ]
 });
 
--- repository/company.js
+++ repository/company.js
@@ -2,12 +2,13 @@
  * Created by zacharyrosenthal on 2/27/16.
  */
 var Company = require('../models/company');
+var Products = require('../models/product');
 
 CompanyRepository = module.exports;
 
 CompanyRepository.getCompanies = function (callback) {
 
-    Company.find().select('name icon').exec( function (err, docs) {
+    Company.find().select('name icon').exec(function (err, docs) {
 
         if (!err) {
 
@@ -22,8 +23,7 @@ CompanyRepository.getCompanies = function (callback) {
 
 CompanyRepository.getCompany = function (name, callback) {
 
-    Company.find({name: name}).populate('products').exec( function (err, document) {
-
+    Company.find({name: name}).populate('products').exec(function (err, document) {
 
         if (!err) {
 
