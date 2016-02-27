7a167d37ea2bcb1bc827601ecb0b93b9c30ebbb9
Ryan Cornillie <cornil15@msu.edu>
2016-02-27 02:26:33 -0500

    like and dislike slider

--- public/js/script.js
+++ public/js/script.js
@@ -5,5 +5,29 @@
 var app = angular.module('myApp', []);
 
 app.controller('myCtrl', function($scope) {
+
+    $scope.getPercent = function() {
+
+        return ($scope.likes/($scope.likes + $scope.dislikes)*100).toString() + "%";
+    };
+
     $scope.test = "angular";
+    $scope.likes = 0;
+    $scope.dislikes = 0;
+    $scope.percent = $scope.getPercent();
+
+
+    $scope.liked = function() {
+
+        $scope.likes++;
+        $scope.percent = $scope.getPercent();
+    };
+
+    $scope.disliked = function() {
+
+        $scope.dislikes++;
+        $scope.percent = $scope.getPercent();
+    };
+
+
 });
