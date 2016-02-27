f314638be66c1d432d99e9369f6b8ed04bbd586a
Ryan Cornillie <cornil15@msu.edu>
2016-02-27 07:02:15 -0500

    companies list

--- public/js/script.js
+++ public/js/script.js
@@ -8,12 +8,18 @@ app.config(function ($routeProvider) {
     $routeProvider.when('/home', {
         templateUrl: '/views/home.html',
         controller: 'myCtrl'
-    }).when('/login', {
+        }).when('/login', {
             templateUrl: '/views/login.html'
-        })
+        }).
 
-        .otherwise({
-            redirectTo: '/home'
+        when('/companies', {
+            templateUrl: '/views/companies.html',
+            controller: 'companiesCtrl'
+        }).
+
+        otherwise({
+            redirectTo: '/companies'
+
         });
 
 });
@@ -29,6 +35,7 @@ app.controller('myCtrl', function ($scope) {
     $scope.likes = 0;
     $scope.dislikes = 0;
     $scope.percent = $scope.getPercent();
+    $scope.comments =[{user:"Zach", comment: "That progress bar is better than anything I have ever programmed"},{user:"Ryan", comment:"Great idea!"}];
 
     $scope.liked = function () {
 
@@ -41,5 +48,28 @@ app.controller('myCtrl', function ($scope) {
         $scope.dislikes++;
         $scope.percent = $scope.getPercent();
     };
+
+    $scope.addComment = function() {
+
+        $scope.comments.push({
+            user: "Ryan",
+            comment: $scope.userComment
+        });
+
+
+    }
+});
+
+
+app.controller('companiesCtrl', function ($scope) {
+
+    $scope.companies =
+        [
+            {name:'Facebook', icon:"fa-facebook-official"},
+            {name:'Twitter', icon: "fa-twitter" },
+            {name:"Slack", icon:"fa-slack"},
+            {name:"Google", icon: "fa-google"},
+            {name: "Amazon", icon:"fa-amazon"}
+        ]
 });
 
