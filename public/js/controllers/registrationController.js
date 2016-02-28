d0a8894ff9ada39f6e48234ea02803a7efc57bc9
Ryan Cornillie <cornil15@msu.edu>
2016-02-28 08:33:42 -0500

    register function in register controller

--- public/js/controllers/registrationController.js
+++ public/js/controllers/registrationController.js
@@ -1,5 +1,12 @@
 app.controller('registrationController', ["$scope", "$routeParams", "productService",
     function ($scope, $routeParams, productService) {
 
+        $scope.register = function(form){
 
+            if(form.$valid){
+
+                console.log("valid form");
+
+            }
+        }
     }]);
