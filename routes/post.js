fc8aa52f976c50535a62db81b345e19fe364da7f
Zachary Rosenthal <rosent76@msu.edu>
2016-02-27 20:44:32 -0500

    goot

--- routes/post.js
+++ routes/post.js
@@ -1,3 +1,4 @@
+
 /**
  * Created by zacharyrosenthal on 2/27/16.
  */
@@ -17,6 +18,25 @@ router.post('/post', function (req, res) {
             res.status(500).send({error: 'object creation failed'});
         }
     });
+});
+
+
+router.post('/vote/', function (req, res) {
+
+    callback = function () {
+
+    };
+
+    //if 1 then upvote
+    if (req.body.direction) {
+
+        comment.upVote(req.body.uId, req.body.pId);
+    } else {
+
+        //downvote
+        comment.downVote(req.body.uId, req.body.pId);
+    }
+
 });
 
 module.exports = router;
